67414543a2b4ac3f41166a80faa355b0caa1ed58
Marie Lacour <marie-lacour@outlook.fr>
2019-05-12 22:36:56 +0200

    Create LogManager.php
    Gestion de l'enregistrement, de la connexion, de la vérification du nom de l'utilisateur

---
+++ blog/model/LogManager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ML\Blog\Model;
+
+require_once("model/Manager.php");
+
+class LogManager extends \ML\Blog\Model\Manager {
+
+    public function userExist($username) {
+        $db = $this->dbConnect();
+        $checkUser = $db->prepare('SELECT * FROM users WHERE username = :username LIMIT 0, 25');
+        $checkUser->execute(array('username' =>  $username));
+        return $checkUser;
+    }
+
+    public function register($username, $password) {
+        $db = $this->dbConnect();
+        $insertUser = $db->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
+        $insertUser->execute(array('username' => $username, 'password' => $password));
+        return $insertUser;
+    }
+
+    public function login($username, $password) {
+        $db = $this->dbConnect();
+        $checkUser = $db->prepare('SELECT id, username, password, status FROM users WHERE username = :username LIMIT 0, 25');
+        $checkUser->execute(array('username' =>  $username));
+        return $checkUser; 
+    }
+}
