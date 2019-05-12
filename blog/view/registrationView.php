9c5656886ce9db8919563d384cccd7c316d2b3b6
Marie Lacour <marie-lacour@outlook.fr>
2019-05-12 22:32:12 +0200

    Create registrationView.php
    Ajout d'un pavé d'enregistrement

---
+++ blog/view/registrationView.php
@@ -0,0 +1,13 @@
+<h1>Bienvenue sur mon blog !</h1>
+
+<div class = "registrationForm">
+
+	<h4>S'inscrire : </h4>
+
+	<form action="index.php?action=register" method="post">
+		<input type="text" name="username" placeholder="Nom d'utilisateur">
+		<input type="password" name="password" placeholder="Mot de passe">
+		<input type="submit" name="inscription" value="S'inscrire">
+	</form>
+	<em><a href="index.php?action=login">Se connecter</a></em>
+</div>
