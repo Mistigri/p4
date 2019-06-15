17d96ea04eeb84a6c2d10e1405afa957d1779480
Marie Lacour <marie-lacour@outlook.fr>
2019-06-15 12:27:02 +0200

    Ajout du tinymce sur l'ajout de post
    Utilisation d'un id pour éviter que la mise en forme ne s'applique à tous les textarea

--- blog/view/template.php
+++ blog/view/template.php
@@ -3,7 +3,10 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="style.css" rel="stylesheet" /> 
+        <link href="public/css/style.css" rel="stylesheet" /> 
+		<!--script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=AIzaSyBvatbqIibckLDXkZknTifQN0ZEmov8j_s"></script-->
+		<script src="https://cdn.tiny.cloud/1/cfrzdwr2883ozk22peorltuqrkcyt8jxiqelelhtmz885mrw/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script> 
+	    <script>tinymce.init({ selector: '#newPost' });</script>
     </head>
 
     <body>
