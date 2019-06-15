91feb60d93cef09a53bd92f1904095e8d018a5f4
Marie Lacour <marie-lacour@outlook.fr>
2019-06-15 11:59:48 +0200

    Update frontend.php
    Mise à jour du pavé connection d'un utilisateur

--- blog/controller/frontend.php
+++ blog/controller/frontend.php
@@ -5,6 +5,8 @@ require_once('model/PostManager.php');
 require_once('model/CommentManager.php');
 require_once('model/LogManager.php');
 
+
+//connexion
 function login($username, $password) {
 	$logUser = new \ML\Blog\Model\LogManager();
 	$checkUser = $logUser->userExist($username);
@@ -16,19 +18,22 @@ function login($username, $password) {
 	        session_start();
 	        $_SESSION['id'] = $checkedUser['id'];
 	        $_SESSION['username'] = $username;
-	        $_SESSION['status'] = $status;
+	        $_SESSION['status'] = $checkedUser['status'];
+
+	        var_dump($checkedUser['status']);
 	        echo 'Vous êtes connecté !';
 	        header('Location: index.php');
 		}
 	    else {
 	        echo 'Erreur : l\'identifiant et/ou le mot de passe ne correspondent pas.';
+	        //header('Location:index.php?action=listPosts');
 	    }
 	}
 	else {
 		header('Location: index.php?action=register&username=' . $username);
 	}
 }
-
+//recherche d'un utilisateur par son pseudo
 function selectUser($username) {
 	$logManager = new \ML\Blog\Model\LogManager();
 	$selectedUser = $logManager->userExist($username);
@@ -40,7 +45,7 @@ function selectUser($username) {
 	}
 	require('view/frontend/connectionView.php');
 }
-
+//enregistrement si inconnu
 function register($username, $password) {
 	$logManager = new \ML\Blog\Model\LogManager();
 	$selectedUser = $logManager->userExist($username);
@@ -49,19 +54,22 @@ function register($username, $password) {
 		throw new Exception('Cet identifiant existe déjà !');
 	}
 	else {
-		var_dump("coucou" .$username. " ".$password);
 		$pass_hache = password_hash($password, PASSWORD_DEFAULT);
 	   	$insertUser = $logManager->register($username,$pass_hache); 
 	}
 	//header('Location: index.php');
 }
 
+
+//accès à la liste des posts
 function listPosts() {
     $postManager = new \ML\Blog\Model\PostManager(); // Création d'un objet
     $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
     require('view/frontend/listPostsView.php');
 }
 
+
+//accès à un post et à ses commentaires
 function post() {
     $postManager = new \ML\Blog\Model\PostManager();
     $commentManager = new \ML\Blog\Model\CommentManager();
@@ -70,8 +78,25 @@ function post() {
     require('view/frontend/postView.php');
 }
 
+function addPost() {
+	require('view/frontend/addPostView.php');
+}
+
+
+function addPost2($post) {
+	$postManager = new \ML\Blog\Model\PostManager();
+	$affectedLines = $postManager->addPost($post);
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le post !');
+    }
+    else {
+        require('view/frontend/listPostView.php');
+    }
+}
+
 function addComment($postId, $author, $comment) {
     $commentManager = new \ML\Blog\Model\CommentManager();
+    $author = $_SESSION['id'];
     $affectedLines = $commentManager->postComment($postId, $author, $comment);
     if ($affectedLines === false) {
         throw new Exception('Impossible d\'ajouter le commentaire !');
@@ -81,16 +106,18 @@ function addComment($postId, $author, $comment) {
     }
 }
 
-function notifyComment($commentId) {
+function notifyComment($commentId/*, $postId*/) {
 	$commentManager = new \ML\Blog\Model\CommentManager();
-	$selectedComment = $commentManager->notifyComment($commentId);
+	$selectedComment = $commentManager->notifyComment($commentId/*, $postId*/);
 	if ($selectedComment === false) {
 	    throw new Exception('Impossible de signaler le commentaire !');
 	}
 	else {
-	    header('Location: index.php?action=notifyComment&id=' . $commentId);
+		//$backToPost = $commentManager -> returnToPost($commentId);
+
+header('Location: index.php');
 	}
-	require('view/frontend/commentView.php');
+	
 }
 
 function selectComment($commentId) {
