62acf1bc3396bf03a3a56e07ad2e981a4faafb0e
Marie Lacour <marie-lacour@outlook.fr>
2019-06-15 12:11:39 +0200

    Ajout de le fonctionnalité ajout de post
    Si l'utilisateur connecté a un statut à 1, il s'agit donc de l'auteur et possibilité pour lui d'ajouter un article ou de modérer les commentaires.

--- blog/view/listPostsView.php
+++ blog/view/listPostsView.php
@@ -1,6 +1,23 @@
-<?php $title = 'Mon blog'; ?>
+<?php 
 
-<?php ob_start(); ?>
+$title = 'Mon blog';
+
+ob_start(); 
+
+if(isset ($_SESSION['username'])) {
+    echo 'Bonjour '.$_SESSION['username']. '.';
+    ?>
+    <div class = "logOut">
+        <em><a href="index.php?action=logOut">Se déconnecter</a></em>
+    </div>
+
+<?php
+}
+else {
+    include("connectionView.php");
+}
+
+?>
 
 <h1>Mon super blog !</h1>
 <p>Derniers billets du blog :</p>
@@ -21,13 +38,23 @@ while ($data = $posts->fetch()) {
     ?>
     <br />
 
-    <em><a href="index.php?id=<?= $data['id'] ?>">Commentaires</a></em>
+    <em><a href="index.php?action=post&id=<?= $data['id'] ?>">Commentaires</a></em>
     </p>
 </div>
 
 <?php
 } // Fin de la boucle des billets
 $posts->closeCursor();
+
+//Ajout de la possibilité d'ajouter un article pour l'administrateur du site
+if(isset($_SESSION['status']) AND ($_SESSION['status']) == 1) {
+    ?>
+    <em><a href="index.php?action=addPost">Ajouter un article</a></em><br/>
+
+    <em><a href="index.php?action=moderateComments">Modérer les commentaires</a></em>
+    <?php
+}
+
 ?>
 
 <?php $content = ob_get_clean(); ?>
