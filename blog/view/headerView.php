b07d4bd0a699c2958b20d5eaed5fe53b2606119e
Marie Lacour <marie-lacour@outlook.fr>
2019-06-15 12:09:40 +0200

    Create headerView.php
    Pavé de connection ou d'enregistrement dans un headerView

---
+++ blog/view/headerView.php
@@ -0,0 +1,30 @@
+
+
+
+<h1>Bienvenue sur mon blog !</h1>
+
+<div class = "connectionForm">
+
+	<h4>Se connecter : </h4>
+
+	<form action="index.php?action=login" method="post">
+		<input type="text" name="username" placeholder="Nom d'utilisateur">
+		<input type="password" name="password" placeholder="Mot de passe">
+		<input type="submit" name="connexion" value="Connexion">
+	</form>
+	<em><a href="index.php?action=register ?>">S'inscrire</a></em>
+
+</div>
+
+<div class = "registrationForm">
+
+	<h4>S'inscrire : </h4>
+
+	<form action="index.php?action=register" method="post">
+		<input type="text" name="username" placeholder="Nom d'utilisateur">
+		<input type="password" name="password" placeholder="Mot de passe">
+		<input type="submit" name="inscription" value="Connexion">
+	</form>
+	<em><a href="index.php?action=login ?>">Se connecter</a></em>
+</div>
+
