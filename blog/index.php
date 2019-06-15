2832f7f950d66717a7664f5925bb7e724d30cdc5
Marie Lacour <marie-lacour@outlook.fr>
2019-06-15 11:42:21 +0200

    Réorganisation des actions
    En fonction du profil : utilisateur non connecté, utilisateur connecté ou administrateur du site.

--- blog/index.php
+++ blog/index.php
@@ -1,34 +1,103 @@
 <?php
 
-require('controller.php');
+session_start();
 
-if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'listPosts') {
-        listPosts();
-    }
-    elseif ($_GET['action'] == 'post') {
-        if (isset($_GET['id_post']) && $_GET['id_post'] > 0) {
-            post();
-        }
-        else {
-            echo 'Erreur : aucun identifiant de billet envoyé';
-        }
-    }
-    elseif ($_GET['action'] == 'addComment') {
-	    if (isset($_GET['id_post']) && $_GET['id_post'] > 0) {
-	        if (!empty($_POST['author']) && !empty($_POST['comment_content'])) {
-	            addComment($_GET['id_post'], $_POST['author'], $_POST['comment_content']);
+require('controller/frontend.php');
+
+try {
+	if (isset($_GET['action'])) {
+
+		if ($_GET['action'] == 'login') {
+	        if (!empty($_POST['username']) && !empty($_POST['password'])) {
+	            login($_POST['username'], ($_POST['password']));
 	        }
 	        else {
-	            echo 'Erreur : tous les champs ne sont pas remplis !';
+	            require("view/frontend/connectionView.php");
+	        }
+	    }	
+
+	    if ($_GET['action'] == 'register') {
+	        if (!empty($_POST['username']) && !empty($_POST['password'])) {
+	            register($_POST['username'], ($_POST['password']));
+	        }
+	        else {
+	            require("view/frontend/registrationView.php");
 	        }
 	    }
-	    else {
-	        echo 'Erreur : aucun identifiant de billet envoyé';
+
+	    if ($_GET['action'] == 'logOut') {
+	    	$_SESSION = array();
+			session_destroy();
+			require("view/frontend/index.php");
 	    }
-    }
+
+	    if ($_GET['action'] == 'listPosts') {
+	        listPosts();
+	    }
+	    elseif ($_GET['action'] == 'post') {
+	        if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+	            post();
+	        }
+	        else {
+	            echo 'Erreur : aucun identifiant de billet envoyé';
+	        }
+	    }
+	    elseif ($_GET['action'] == 'addComment') {
+		    if (isset($_GET['id']) && $_GET['id'] > 0) {
+		        if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+		            addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+		        }
+		        else {
+		            echo 'Erreur : tous les champs ne sont pas remplis !';
+		        }
+		    }
+		    else {
+		        echo 'Erreur : aucun identifiant de billet envoyé';
+		    }
+	    }
+	    //signaler des commentaires
+	    elseif ($_GET['action'] == 'notifyComment') {
+	    	if (isset($_GET['id']) && $_GET['id'] > 0) {
+	    		notifyComment($_GET['id']);
+        	}
+        	else {
+        		echo 'Erreur : aucun identifiant de commentaire envoyé';
+        	}	
+	    }
+
+		/*partie modif commentaire
+	    elseif ($_GET['action'] == 'modifyComment') {
+	    	if (isset($_GET['id']) && $_GET['id'] > 0) {
+	    		selectComment($_GET['id']);
+        	}
+        	else {
+        		echo 'Erreur : aucun identifiant de commentaire envoyé';
+        	}	
+	    }
+
+	    elseif ($_GET['action'] == 'updateComment') {
+	    	if (isset($_GET['id']) && $_GET['id'] > 0) {
+				if (!empty($_POST['newAuthor']) && !empty($_POST['newComment'])) {
+	            	modifyComment($_GET['id'], $_GET['postid'], $_POST['newAuthor'], $_POST['newComment']);
+	        	}
+	        	else {
+	            	echo 'Erreur : tous les champs ne sont pas remplis !';
+	        	}
+	    	}
+	    }*/
+
+	}
+
+	else {
+	    listPosts();
+	}	
 }
 
-else {
-    listPosts();
+catch(Exception $e) { 
+
+    echo 'Erreur : ' . $e->getMessage();
+
 }
+
+
+
