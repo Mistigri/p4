dd8539ecc1cd6de0014769bbf8b7647e75401428
Marie Lacour <marie-lacour@outlook.fr>
2019-08-21 22:44:30 +0200

    Update template.php
    Ajout de 2 includes pour le header et le footer. le premier pour le pavé de connection, et le second pour les mentions légales.

--- blog/view/template.php
+++ blog/view/template.php
@@ -2,14 +2,34 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title><?= $title ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet">
         <link href="public/css/style.css" rel="stylesheet" /> 
-		<!--script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=AIzaSyBvatbqIibckLDXkZknTifQN0ZEmov8j_s"></script-->
 		<script src="https://cdn.tiny.cloud/1/cfrzdwr2883ozk22peorltuqrkcyt8jxiqelelhtmz885mrw/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script> 
 	    <script>tinymce.init({ selector: '#newPost' });</script>
+        <title><!--?= $title ?-->Mon blog</title>
     </head>
 
     <body>
-        <?= $content ?>
+
+
+        <div class="container-fluid">
+            <header>
+                <?php include("headerView.php"); ?>
+            </header>
+            <section>
+                <?= $content ?>
+            </section>
+            <footer>
+                <?php include("footerView.php"); ?>
+            </footer--> 
+        </div>
+        
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
