1ccd419a9c92bd57befd266fd26498a41bde806d
Marie Lacour <marie-lacour@outlook.fr>
2019-08-21 22:40:30 +0200

    Update headerView.php
    Réorganisation du contenu des fichiers

--- blog/view/headerView.php
+++ blog/view/headerView.php
@@ -1,26 +1,24 @@
-<h1>Bienvenue sur mon blog !</h1>
-
-<div class = "connectionForm">
-	<h4>Se connecter : </h4>
-
-	<form action="index.php?action=login" method="post">
-		<input type="text" name="username" placeholder="Nom d'utilisateur">
-		<input type="password" name="password" placeholder="Mot de passe">
-		<input type="submit" name="connexion" value="Connexion">
-	</form>
-
-	<em><a href="index.php?action=register ?>">S'inscrire</a></em>
-</div>
-
-<div class = "registrationForm">
-	<h4>S'inscrire : </h4>
-
-	<form action="index.php?action=register" method="post">
-		<input type="text" name="username" placeholder="Nom d'utilisateur">
-		<input type="password" name="password" placeholder="Mot de passe">
-		<input type="submit" name="inscription" value="Connexion">
-	</form>
-
-	<em><a href="index.php?action=login ?>">Se connecter</a></em>
-</div>
-
+<?php
+if(isset ($_SESSION['username'])) {
+    ?>
+    <div class="welcomeUser">
+        Bonjour <?= htmlspecialchars($_SESSION['username']); ?>.
+        <div class = "logOut">
+            <em><a href="index.php?action=logOut">Se déconnecter</a></em>
+        </div>
+    </div>
+<?php
+}
+else {
+    //require("connectionView.php");
+    ?>	
+    <div class = "connectionForm row justify-content-end">
+		<form action="index.php?action=login" method="post">
+			<input type="text" name="username" placeholder="Nom d'utilisateur">
+			<input type="password" name="password" placeholder="Mot de passe">
+			<button type="submit" class="btn btn-info">Se connecter</button>
+		</form>
+		<span class="sinscrire"><em><a href="index.php?action=register">S'inscrire</a></em></span>
+	</div>
+<?php
+}
