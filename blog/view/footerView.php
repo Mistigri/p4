4e01641a113df06771eef2828815722f528958d9
Marie Lacour <marie-lacour@outlook.fr>
2019-08-21 22:44:59 +0200

    Create footerView.php
    Ajout d'un fichier pour les mentions légales

---
+++ blog/view/footerView.php
@@ -0,0 +1,13 @@
+<footer>
+    <span class="mentionsLegales">Site développé par :</span> <br/>
+    Marie Lacour<br/>
+    35 rue des Déportés du 11 novembre 43<br/>
+    38100 Grenoble<br/>
+    06 12 13 14 15<br/>
+    marie-lacour@outlook.fr<br/>
+    dans le cadre d'un projet OpenClassrooms.<br/>
+    <img 
+        src="./public/images/logo_OpenClassrooms.png"
+        alt="[Logo d'OpenClassrooms]"
+        /><br/>
+</footer>
