e8e2105a930ab0aef36bbab0a42057d8d611474d
Marie Lacour <marie-lacour@outlook.fr>
2019-09-08 18:43:53 +0200

    Update footerView.php
    Mise à jour contenu

--- blog/view/footerView.php
+++ blog/view/footerView.php
@@ -1,10 +1,11 @@
-<footer>
-    <span class="mentionsLegales">Site développé par :</span> <br/>
-    Marie Lacour<br/>
-    35 rue des Déportés du 11 novembre 43<br/>
-    38100 Grenoble<br/>
-    06 12 13 14 15<br/>
-    marie-lacour@outlook.fr<br/>
-    dans le cadre d'un projet OpenClassrooms.<br/>
-    <img src="./public/images/logo_OpenClassrooms.png" alt="[Logo d'OpenClassrooms]"/><br/>
-</footer>
+<span class="mentionsLegales">Site développé par :</span> <br/>
+Marie Lacour<br/>
+35 rue des Nains de Jardins<br/>
+38100 Grenoble<br/>
+06 12 13 14 15<br/>
+marie-lacour@outlook.fr<br/>
+dans le cadre d'un projet OpenClassrooms.<br/>
+<img class="logo" 
+    src="./public/images/logo_OpenClassrooms.png"
+    alt="[Logo d'OpenClassrooms]"
+    /><br/>
