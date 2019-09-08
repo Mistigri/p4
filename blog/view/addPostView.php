d2f3c95b155373b8c9ff9616d81e6a4b52decd02
Marie Lacour <marie-lacour@outlook.fr>
2019-09-08 18:37:22 +0200

    Update addPostView.php
    Amélioration du design des boutons de la vue

--- blog/view/addPostView.php
+++ blog/view/addPostView.php
@@ -1,31 +1,21 @@
-<?php 
-
-ob_start(); 
-
-?>
+<?php ob_start(); ?>
 
 <?php 
-
 if(isset($_SESSION['id']) && ($_SESSION['status'] == 1)) {
 ?>    
     <p><a href="index.php">Retour à la liste des billets</a></p>
 
     <div class="Ajouter un post :">
         <form action = "index.php?action=formPost" method="POST">
-            <label for ="titre">Titre : </label><input type = "text" id="titlePost" name="titlePost"><br/>
+            <label for ="titre">Titre : </label><input type="text" id="titlePost" name="titlePost" class = "titlePost"><br/>
             <label for = "texte">Texte : </label><textarea id ="newPost" name= "newPost"></textarea><br/>
-            <input type ="submit" value = "Enregistrer">
+            <button type="submit" class="btn btn-info enregistrement">Enregistrer</button>
         </form>    
     </div>
-
 <?php
 }
 ?>
 
-<?php
-
-$content = ob_get_clean(); 
-
-?>
+<?php $content = ob_get_clean(); ?>
  
 <?php require('template.php'); ?>
