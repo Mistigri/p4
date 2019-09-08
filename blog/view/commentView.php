50bb9dd0160e0eff163fd501f48d5be67a3b8d1c
Marie Lacour <marie-lacour@outlook.fr>
2019-09-08 18:37:51 +0200

    Update commentView.php
    Suppression commentaires inutiles

--- blog/view/commentView.php
+++ blog/view/commentView.php
@@ -1,6 +1,4 @@
-<?php 
-
-ob_start(); ?>
+<?php ob_start(); ?>
 
 <h1>Mon super blog !</h1>
 <p><a href="index.php">Retour à la liste des billets</a></p>
@@ -20,7 +18,6 @@ $selectComment=$selectedComment->fetch();
 	</div>
 </form>
 
-<?php
-$content = ob_get_clean(); ?>
+<?php $content = ob_get_clean(); ?>
  
 <?php require('template.php'); ?>
